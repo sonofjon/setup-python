84caf4397ff816d45b5abe2e17fcbe62b0117d92
Andreas Jonsson <ajdev8@gmail.com>
2024-03-04 12:02:00 +0100

    Add general pip commands

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+## Pip
+
+pip install pip-autoremove      # alternatives: pip3-autoremove,
+                                # python3-pip-autoremove,
+                                # only-pip3-autoremove
+pip install pip-search
+pip install pipdeptree          # alternaives: deptree, johnnydep, pipgrip, pip-tree
+pip install --no-deps pipreqs
+pip install yarg docopt         # dependecies for pipreqs
+
+## Python
+
 # pip install autopep8
 # pip install black
 # pip install flake8
