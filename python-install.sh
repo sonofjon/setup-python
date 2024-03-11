c376cee7312f14c33e06f1205a4bea6a4f107ee2
Andreas Jonsson <ajdev8@gmail.com>
2024-03-11 22:14:32 +0100

    Use pip3

--- python-install.sh
+++ python-install.sh
@@ -1,32 +1,32 @@
 ### Pip
 
-pip install pip-autoremove      # alternatives: pip3-autoremove,
+pip3 install pip-autoremove      # alternatives: pip3-autoremove,
                                 # python3-pip-autoremove,
                                 # only-pip3-autoremove
-pip install pip-search
+pip3 install pip-search
 
-pip install pipdeptree          # alternaives: deptree, johnnydep,
+pip3 install pipdeptree          # alternaives: deptree, johnnydep,
                                 # pipgrip, pip-tree
-pip install --no-deps pipreqs
-pip install yarg docopt         # dependecies for pipreqs
+pip3 install --no-deps pipreqs
+pip3 install yarg docopt         # dependecies for pipreqs
 ### LSP
 
 ## python-lsp-server (pylsp)
-# pip install python-lsp-server[all]
-pip install python-lsp-server
-# pip install python-lsp-black
-# pip install python-lsp-isort
-pip install python-lsp-ruff
-pip install pylsp-rope
+# pip3 install python-lsp-server[all]
+pip3 install python-lsp-server
+# pip3 install python-lsp-black
+# pip3 install python-lsp-isort
+pip3 install python-lsp-ruff
+pip3 install pylsp-rope
 
 ## Ruff LSP
-pip install ruff-lsp
+pip3 install ruff-lsp
 
 ### Lint
 
-# pip install autopep8
-# pip install black
-# pip install flake8
-# pip install pylint
-pip install ruff
+# pip3 install autopep8
+# pip3 install black
+# pip3 install flake8
+# pip3 install pylint
+pip3 install ruff
 
